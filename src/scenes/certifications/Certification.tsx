c91512ba4c47640f7329d4c1eb8caa6e08ff7fb9
Zhuofan-Zhang <zhangzhuofan2019@163.com>
2023-04-02 20:36:45 +0800

    change class style

--- src/scenes/certifications/Certification.tsx
+++ src/scenes/certifications/Certification.tsx
@@ -1,24 +1,19 @@
+import useMediaQuery from "../../shared/helpers";
+
 type Props = {
-  name: string;
-  description?: string;
-  image: string;
+    name: string;
+    description?: string;
+    image: string;
 };
 
-const Certification = ({ name, description, image }: Props) => {
-  const overlayStyles = `p-5 absolute z-30 flex
-  h-[380px] w-[450px] flex-col items-center justify-center
-  whitespace-normal bg-primary-500 text-center text-white
-  opacity-0 transition duration-500 hover:opacity-90`;
+const Certification = ({name, image}: Props) => {
 
-  return (
-    <li className="relative mx-5 inline-block h-[380px] w-[450px]">
-      <div className={overlayStyles}>
-        <p className="text-2xl">{name}</p>
-        <p className="mt-5">{description}</p>
-      </div>
-      <img alt={`${image}`} src={image} />
-    </li>
-  );
+    return (
+        <li className="relative mx-5 flex-col items-center justify-center inline-block h-[380px] w-[450px]">
+            <div className="text-base pb-3">{name}</div>
+            <img alt={`${image}`} src={image}/>
+        </li>
+    );
 };
 
 export default Certification;
