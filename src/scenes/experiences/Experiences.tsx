d4b442a6a9fabbe3bd9d236ae4deb5ca045c02d1
Zhuofan-Zhang <zhangzhuofan2019@163.com>
2023-04-02 16:21:08 +0800

    fix onViewPort Enter for experience

--- src/scenes/experiences/Experiences.tsx
+++ src/scenes/experiences/Experiences.tsx
@@ -3,6 +3,7 @@ import {SelectedPage} from '@/shared/types';
 import useMediaQuery from "../../shared/helpers";
 import ExperiencesTimeline from "./ExperiencesTimeline";
 import StepsComponent from "./StepsComponent";
+import {motion} from 'framer-motion';
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
@@ -12,16 +13,18 @@ const Experiences: React.FC<Props> = ({setSelectedPage}: Props) => {
     const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
 
     return (
-        <section id="experiences" className=" md:h-full">
-            {isAboveMediumScreens ?
-                (
-                    <ExperiencesTimeline setSelectedPage={setSelectedPage}/>
-                )
-                : (
-                    <div>
-                        <StepsComponent setSelectedPage={setSelectedPage}/>
-                    </div>)
-            }
+        <section id="experiences" className=" md:h-full py-40">
+            <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.Experiences)}>
+                {isAboveMediumScreens ?
+                    (
+                        <ExperiencesTimeline setSelectedPage={setSelectedPage}/>
+                    )
+                    : (
+                        <div>
+                            <StepsComponent setSelectedPage={setSelectedPage}/>
+                        </div>)
+                }
+            </motion.div>
         </section>
     );
 };
--- src/scenes/experiences/StepsComponent.tsx
+++ src/scenes/experiences/StepsComponent.tsx
@@ -11,35 +11,39 @@ const {Step} = Steps;
 
 const StepsComponent: React.FC<Props> = ({setSelectedPage}: Props) => {
     return (
-        <motion.div
-            className="mx-auto w-5/6 gap-5 items-center justify-center md:flex md:h-full pt-20"
-            initial="hidden"
-            whileInView="visible"
-            viewport={{once: true, amount: 0.5}}
-            transition={{duration: 0.5}}
-            variants={{
-                hidden: {opacity: 0, x: -50},
-                visible: {opacity: 1, x: 0},
-            }}>
-            <div
-                className="mx-auto text-2xl w-5/6 gap-5 items-center justify-center md:flex md:h-full pb-10">Education
-            </div>
-            <Steps progressDot current={educationSteps.length} direction="horizontal" responsive={false} size="small"
-                   className="whitespace-nowrap md:flex md:h-full w-5/6 pb-20"
-                   style={{overflowX: "scroll", paddingBottom: "20px"}}>
-                {educationSteps.map((step, index) => (
-                    <Step className="step-content" key={index} title={step.title} description={step.description}/>
-                ))}
-            </Steps>
-            <div className="mx-auto text-2xl w-5/6 gap-5 items-center justify-center md:flex md:h-full pb-10">Work</div>
-            <Steps progressDot current={workSteps.length} direction="horizontal" responsive={false} size="small"
-                   className="whitespace-nowrap md:flex md:h-full w-5/6"
-                   style={{overflowX: "scroll", paddingBottom: "20px"}}>
-                {workSteps.map((step, index) => (
-                    <Step className="step-content" key={index} title={step.title} subTitle={step.position}
-                          description={step.description}/>
-                ))}
-            </Steps>
+        <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.Experiences)}>
+            <motion.div
+                className="mx-auto w-5/6 gap-5 items-center justify-center md:flex md:h-full"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{once: true, amount: 0.5}}
+                transition={{duration: 0.5}}
+                variants={{
+                    hidden: {opacity: 0, x: -50},
+                    visible: {opacity: 1, x: 0},
+                }}>
+                <div
+                    className="mx-auto text-2xl w-5/6 gap-5 items-center justify-center md:flex md:h-full pb-10">Education
+                </div>
+                <Steps progressDot current={educationSteps.length} direction="horizontal" responsive={false}
+                       size="small"
+                       className="whitespace-nowrap md:flex md:h-full w-5/6 pb-20"
+                       style={{overflowX: "scroll", paddingBottom: "20px"}}>
+                    {educationSteps.map((step, index) => (
+                        <Step className="step-content" key={index} title={step.title} description={step.description}/>
+                    ))}
+                </Steps>
+                <div className="mx-auto text-2xl w-5/6 gap-5 items-center justify-center md:flex md:h-full pb-10">Work
+                </div>
+                <Steps progressDot current={workSteps.length} direction="horizontal" responsive={false} size="small"
+                       className="whitespace-nowrap md:flex md:h-full w-5/6"
+                       style={{overflowX: "scroll", paddingBottom: "20px"}}>
+                    {workSteps.map((step, index) => (
+                        <Step className="step-content" key={index} title={step.title} subTitle={step.position}
+                              description={step.description}/>
+                    ))}
+                </Steps>
+            </motion.div>
         </motion.div>
     );
 };
